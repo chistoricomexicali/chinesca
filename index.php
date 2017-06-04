cf159f6833ce3348895a0932e37a4aaa65c64ea7
chistoricomexicali <chistoricomexicali@gmail.com>
2017-06-04 19:35:11 -0400

    segundo commit

--- index.php
+++ index.php
@@ -1,10 +1,19 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
   <meta charset="UTF-8">
-  <title>Document</title>
+  <title>Centro Histórico de Mexicali.</title>
 </head>
 <body>
-  <h1>Yeah chikito!</h1>
+  <img src="img/logo.png" alt="">
+  <div align="center">
+    <h1>¡BIENVENIDOS A LA PÁGINA DEL CENTRO HISTÓRICO DE MEXICALI!</h1>
+    <img src="http://www.zonalider.com/sites/default/files/styles/scale_max_width_auto_height/public/article/image/nuevo_centro_historico_de_mexicali.jpg" alt="">
+    <h1>
+      Propuesta de Template para página <a href="assets/PerfectTravel">Click aquí..</a>
+    </h1>
+    <br>
+    <h1>=)</h1>
+  </div>
 </body>
 </html>
